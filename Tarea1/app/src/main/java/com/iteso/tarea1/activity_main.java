80ef4c85fc0547e43c25eb86832910984a8e3a71
Toño Romero <to_r_m@hotmail.com>
2018-09-30 19:52:27 -0500

    push 2

--- Tarea1/app/src/main/java/com/iteso/tarea1/activity_main.java
+++ Tarea1/app/src/main/java/com/iteso/tarea1/activity_main.java
@@ -1,10 +1,8 @@
 package com.iteso.tarea1;
 import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -15,126 +13,123 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-public class activity_main extends AppCompatActivity implements
-        AdapterView.OnItemSelectedListener{
-    public static String escolaridad;
 
-    Button limpiar;
-    Spinner s_escolaridad;
-    EditText nombre;
-    EditText telefono;
-    String genero_persona = "Femenino";
-    String deporte_string = "No practica deporte";
+public class activity_main extends AppCompatActivity  implements AdapterView.OnItemSelectedListener{
+    public static String s_escolaridad;
+    EditText nombre, telefono;
+    Spinner escolaridad;
     RadioGroup genero;
-    CheckBox deporte;
     AutoCompleteTextView libro;
-
-
+    CheckBox deporte_chk;
+    String genero_persona = "Femenino", deporte = "No practica deporte";
+    Button limpiar;
+    RadioButton Femenino, Masculino;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        limpiar = findViewById(R.id.activity_main_limpiar);
-        s_escolaridad = findViewById(R.id.activity_main_escolaridad);
+        Femenino = findViewById(R.id.activity_main_genero_f);
+        Masculino = findViewById(R.id.activity_main_genero_m);
         nombre = findViewById(R.id.activity_main_name_editar_texto);
-        telefono = findViewById(R.id.activity_main_phone_editar_texto);
+        telefono = findViewById(R.id.activity_main_telefono_editar_texto);
+        escolaridad = findViewById(R.id.activity_main_escolaridad);
         genero = findViewById(R.id.activity_main_genero);
-        deporte = findViewById(R.id.activity_main_deporte);
         libro = findViewById(R.id.activity_main_libro);
-
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(activity_main.this, R.array.Escolaridades,  android.R.layout.simple_spinner_item);
+        deporte_chk = findViewById(R.id.activity_main_deporte);
+        limpiar = findViewById(R.id.activity_main_limpiar);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(activity_main.this, R.array.Escolaridades, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        s_escolaridad.setAdapter(adapter);
-        s_escolaridad.setOnItemSelectedListener(this);
-
-        ArrayAdapter<CharSequence> adapterComplete = ArrayAdapter.createFromResource(activity_main.this, R.array.Libros  , android.R.layout.simple_dropdown_item_1line);
+        escolaridad.setAdapter(adapter);
+        escolaridad.setOnItemSelectedListener(this);
+        ArrayAdapter<CharSequence> adapterComplete = ArrayAdapter.createFromResource(activity_main.this, R.array.Libros, android.R.layout.simple_dropdown_item_1line);
         libro.setAdapter(adapterComplete);
         libro.setOnItemSelectedListener(this);
         limpiar.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View view) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(activity_main.this);
-                builder.setMessage(R.string.activity_main_texto_dialogo).setPositiveButton(R.string.activity_main_texto_aceptar, new DialogInterface.OnClickListener() {
-
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        nombre.setText("");
-                        telefono.setText("");
-                        libro   .setText("");
-                        deporte.setChecked(false);
-                    }
-                })
-                        .setNegativeButton(R.string.activity_main_texto_cancelar,
-                                new DialogInterface.OnClickListener() {
-
+                builder.setMessage(R.string.activity_main_texto_dialogo).setPositiveButton(R.string.activity_main_texto_aceptar,
+                        new DialogInterface.OnClickListener() {
                             @Override
-                            public void onClick(DialogInterface dialogInterface, int i){}}).show();
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                nombre.setText("");
+                                telefono.setText("");
+                                libro.setText("");
+                                deporte_chk.setChecked(false);
+                                Femenino.setChecked(true);
+                                Masculino.setChecked(false);
+                                escolaridad.setSelection(0);
+                            }
+                        })
+                        .setNegativeButton(R.string.activity_main_texto_cancelar, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //do nothing
+                            }
+                        }).show();
+
             }
         });
     }
-
-    public boolean onCreateOptionsMenu(Menu menu){ MenuInflater inflater = getMenuInflater();
-    inflater.inflate(R.menu.activity_main, menu);
-    return true;
-    }
-    @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l)
-    {
-        escolaridad = adapterView.getItemAtPosition(i).toString();
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_main, menu);
+        return true;
     }
     public boolean onOptionsItemSelected (MenuItem item){
         switch (item.getItemId()){
             case(R.id.menu_guardar):
-
                 Toast.makeText(activity_main.this,
                         "Nombre: "
                                 + nombre.getText().toString()
                                 + "\nTelefono: "
                                 + telefono.getText().toString()
                                 + "\nEscolaridad: "
-                                + escolaridad
+                                + s_escolaridad
                                 + "\nGénero: "
                                 + genero_persona
                                 + "\nLibro favorito: "
-                                + libro.getText().toString()
-                                + "\n"
-                                + deporte_string,
-                        Toast.LENGTH_SHORT).show();
-                Log.d("save menu","contenido de name : " + nombre.toString());
+                                + libro.getText().toString() + "\n"
+                                + deporte.toString()
+                        , Toast.LENGTH_SHORT).show();
                 return true;
-                default:
-                    return super.onOptionsItemSelected(item);
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
-                    public void RbtnClick(View view){
-        switch
-                (view.getId()){
-            case (R.id.activity_main_genero_f):
-                genero_persona = "Femenino";
-            break;
+    public void ChkClicked(View view){
+        boolean checked = deporte_chk.isChecked();
+        switch (view.getId()){
+            case (R.id.activity_main_deporte):
+                if(checked)
+                    deporte  = "Si practica deporte";
+                else
+                    deporte= "No practica deporte";
+                break;
+            default:
+                deporte = "No practica deporte";
+        }
+    }
+    public void RbtnClicked(View view){
+        switch (view.getId()){
             case (R.id.activity_main_genero_m):
                 genero_persona = "Masculino";
-            break;
+                break;
+            case (R.id.activity_main_genero_f):
+                genero_persona = "Femenino";
+                break;
         }
     }
-    public void onCheckBoxClicked (View view){ boolean checked = deporte.isChecked();
-    switch (view.getId()){
-        case (R.id.activity_main_deporte):
-            if(checked) deporte_string = "Si practica deporte";
-            else deporte_string = "No practica deporte";
-            break;
-            default: deporte_string = "No practica deporte";
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        s_escolaridad = adapterView.getItemAtPosition(i).toString();
     }
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
     }
-
-
 }
